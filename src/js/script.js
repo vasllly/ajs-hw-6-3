73dd623ce474966be162a66c99ca21e937ca1b9e
Ganja Vasiliy <vasllly219@gmail.com>
2019-03-21 21:18:16 +0300

    FIX

--- src/js/script.js
+++ src/js/script.js
@@ -1,26 +1,11 @@
 export default function getDescription(object) {
-  let avatar;
-  switch (object.type) {
-    case 'Bowman':
-      avatar = '\u{1F620}';
-      break;
-    case 'Swordsman':
-      avatar = '\u{1F621}';
-      break;
-    case 'Magician':
-      avatar = '\u{1F9D0}';
-      break;
-    case 'Daemon':
-      avatar = '\u{1F47F}';
-      break;
-    case 'Undead':
-      avatar = '\u{1F480}';
-      break;
-    case 'Zombie':
-      avatar = '\u{1F9DF}';
-      break;
-    default:
-      avatar = '';
-  }
-  return `${avatar} ${object.name.charAt(0)}(${object.level}) \u{2694}${object.attack} \u{1F6E1}${object.defence} \u{2764}${object.health}`;
+  const avatars = {
+    Bowman: '\u{1F620}',
+    Swordsman: '\u{1F621}',
+    Magician: '\u{1F9D0}',
+    Daemon: '\u{1F47F}',
+    Undead: '\u{1F480}',
+    Zombie: '\u{1F9DF}',
+  };
+  return `${avatars[object.type] ? avatars[object.type] : ''} ${object.name.charAt(0)}(${object.level}) \u{2694}${object.attack} \u{1F6E1}${object.defence} \u{2764}${object.health}`;
 }
